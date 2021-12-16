7055364aaabfeea2e576aa5c533f8f3f537f7fea
Toomas <toomas.viirsalu@glia.com>
2021-12-16 18:23:55 +0200

    Update short script version

--- midwest/phone-removal-tv.js
+++ midwest/phone-removal-tv.js
@@ -1,7 +1,7 @@
 // long version - separate behaviour depending on class
-const messagePhone = `<span data-sm-show-media-selection-on="mouseover">Contact Us!</span>`;
-const messagePhoneLink = `<a data-sm-show-media-selection-on="click" href="javascript:void(0);">Contact Us!</a>`;
-const messageContactAny = `<a data-sm-show-media-selection-on="click" href="javascript:void(0);">Contact Us!</a>`;
+const messagePhone = `<span data-sm-show-media-selection-on="mouseover">Contact Us</span>`;
+const messagePhoneLink = `<a data-sm-show-media-selection-on="click" href="javascript:void(0);">Contact Us</a>`;
+const messageContactAny = `<a data-sm-show-media-selection-on="click" href="javascript:void(0);">Contact Us</a>`;
 const phoneNumArr = Array.from(document.body.querySelectorAll(".phone-num"));
 const phoneLinkArr = Array.from(document.body.querySelectorAll(".phone-number-link"));
 const contactAnyArr = Array.from(document.body.querySelectorAll(".contact-any"));
@@ -81,32 +81,67 @@ sm.getApi({version: 'v1'}).then((glia) => {
 
 
 /////// Short version ////////
-const message = `<a data-sm-show-media-selection-on="click" href="javascript:void(0);">Contact Us!</a>`;
+const message = '<a data-sm-show-media-selection-on="click" href="javascript:void(0);">Contact Us</a>';
 const phoneArr = Array.from(document.body.querySelectorAll(".phone-number"));
 
 sm.getApi({version: 'v1'}).then((glia) => {
-  //
   const onQueueStateUpdate = (queueState) => {
-    // Other phone numbers are replaced when a queue with audio or phone ability is available.
-    // The phone numbers are changed back when no 
+    // Replace phone number elements with the control when a queue with audio or phone ability is available.
     if (queueState.state === queueState.QUEUE_STATES.CAN_QUEUE &&
-        (queueState.medias.indexOf('audio') >= 0 || queueState.medias.indexOf('phone') >= 0)) {
-      // replace phone numbers with Contact Us text
+        (queueState.medias.includes('audio') || 
+         queueState.medias.includes('phone'))) {
+      // Replace all the phone numbers with the Contact Us control.
       phoneArr.forEach((elem) => {
         elem.innerHTML = message;
       });
-    } else if (queueState.state === queueState.QUEUE_STATES.CANNOT_QUEUE) {
-//    || (queueState.medias.indexOf('audio') === -1 && queueState.medias.indexOf('phone') === -1) ) {
-      // replace Contact Us text with phone numbers
+    // Restore the original phone numbers when no queue with audio or phone media is available.
+    } else if (queueState.state === queueState.QUEUE_STATES.CANNOT_QUEUE ||
+               !(queueState.medias.includes('audio') ||
+                 queueState.medias.includes('phone'))) {
+      // Replace the Contact Us controls with the original phone numbers.
       phoneArr.forEach((elem) => {
-        elem.innerHTML = elem.getAttribute("glia-phone-orig");
+        elem.innerHTML = elem.getAttribute("glia-phone-original");
       });
     }
   }
-  // Store the initial situation so it can be replaced back and forth
+
+  // Store the initial situation to be able to replace it back and forth.
   phoneArr.forEach((elem) => {
-    elem.setAttribute("glia-phone-orig", elem.innerHTML);
+    elem.setAttribute("glia-phone-original", elem.innerHTML);
   });
-  // Add listener to act in case any queue state is changed
+  
+  // Add listener to act in case any queue state is changed.
   glia.addEventListener(glia.EVENTS.QUEUE_STATE_UPDATE, onQueueStateUpdate);
 });
+
+
+////// Version where onQueueStateUpdate function is defined independently
+  // const message = `<a data-sm-show-media-selection-on="click" href="javascript:void(0);">Contact Us</a>`;
+  // const phoneArr = Array.from(document.body.querySelectorAll(".phone-number"));
+  // // Store the initial situation so it can be replaced back and forth
+  // phoneArr.forEach((elem) => {
+  //   elem.setAttribute("glia-phone-orig", elem.innerHTML);
+  // });
+  // // zzzzz
+  // const onQueueStateUpdate = (queueState) => {
+  //   // Other phone numbers are replaced when a queue with audio or phone ability is available.
+  //   // The phone numbers are changed back when no 
+  //   if (queueState.state === queueState.QUEUE_STATES.CAN_QUEUE &&
+  //       (queueState.medias.indexOf('audio') >= 0 || queueState.medias.indexOf('phone') >= 0)) {
+  //     // replace phone numbers with Contact Us text
+  //     phoneArr.forEach((elem) => {
+  //       elem.innerHTML = message;
+  //     });
+  //   } else if (queueState.state === queueState.QUEUE_STATES.CANNOT_QUEUE) {
+  // //    || (queueState.medias.indexOf('audio') === -1 && queueState.medias.indexOf('phone') === -1) ) {
+  //     // replace Contact Us text with phone numbers
+  //     phoneArr.forEach((elem) => {
+  //       elem.innerHTML = elem.getAttribute("glia-phone-orig");
+  //     });
+  //   }
+  // }
+  // // xxxxx
+  // sm.getApi({version: 'v1'}).then((glia) => {
+  //   // Add listener to act in case any queue state is changed
+  //   glia.addEventListener(glia.EVENTS.QUEUE_STATE_UPDATE, onQueueStateUpdate);
+  // });
